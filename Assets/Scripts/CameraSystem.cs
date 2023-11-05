832b9bf17987af89298ea0331ec10cb39f64877f
Sam Buckley <1029099@student.sae.edu.au>
2023-11-06 09:31:42 +1000

    Camera 2nd commit

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -9,11 +9,27 @@ public class CameraSystem : MonoBehaviour
     public Vector3 offset;
     public float damping;
 
+    public Rigidbody2D playerRB;
+
+    public float playerZoom;
+
+    public float zoomFactor;
+
+    public Camera OrthographicCamera;
+
     private Vector3 velocity = Vector3.zero;
 
-    private void FixedUpdate()
+    public void Awake()
+    {
+        OrthographicCamera = GetComponent<Camera>();
+        playerZoom = 10f;
+    }
+
+    private void Update()
     {
         Vector3 movePosition = playerTransform.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+        playerZoom = playerRB.velocity.magnitude / 10 + 8f;
+        OrthographicCamera.orthographicSize = playerZoom;
     }
 }
