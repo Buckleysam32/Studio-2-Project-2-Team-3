a27959e33175610f8b89b99d10fe5c909d224fbf
Aatkins2002 <1022512@student.sae.edu.au>
2023-11-19 20:30:40 +1000

    Dynamic Spawn V2 - Spawn points are scattered across the map, they each can spawn a random object from a personal list with a personal rarity. Now it just needs the hazard objects themselves.

--- Assets/Scripts/DynamSpawn/DynamDespawn.cs
+++ Assets/Scripts/DynamSpawn/DynamDespawn.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class DynamDespawn : MonoBehaviour
 {
     /// <summary>
-    /// Despawn objects it comes into contact with if that object has the tag "Dynamic"
+    /// Despawn objects it comes into contact with if that object has the tag "Dynamic" & clears up spawners
     /// </summary>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Dynamic"))
+        if (collision.CompareTag("Dynamic"))
         {
             Destroy(collision.gameObject);
         }
+        else if (collision.GetComponent<DynamPrefabSpawn>())
+        {
+            DynamPrefabSpawn changeBool;
+            changeBool= collision.GetComponent<DynamPrefabSpawn>();
+            changeBool.occupied = false;
+        }
     }
+
 }
--- Assets/Scripts/DynamSpawn/DynamPrefabSpawn.cs
+++ Assets/Scripts/DynamSpawn/DynamPrefabSpawn.cs
@@ -4,83 +4,57 @@ using UnityEngine;
 
 public class DynamPrefabSpawn : MonoBehaviour
 {
-    [SerializeField] GameObject Object; //The prefab we intend to spawn
-    public int spawnRarity = 5; //How rare it is, smaller number means it spawns more often
-    public float spawnDelay = 2; // How long to wait between each attempt to spawn
-    private int spawnTarget = 0;
-    private bool freeze = true;
-    private float timePass = 0f;
-    [SerializeField] float cameraEdgeX = 9f; //The camera size for the X axis (left and right)
-    [SerializeField] float cameraEdgeY = 6f; //The camera size for the Y axis (up and down)
+    [SerializeField] GameObject[] objectList; //The prefabs we intend to spawn, will pick a random one from this list. Make sure they're dynamic
+    [SerializeField] int spawnRarity = 5; //How rare it is, smaller number means it spawns more often. 1 means always spawn. WANRING: IF 0 IT WILL NOT SPAWN
+    private int spawnTarget = 1;
+    public bool eligable = false; //the DynamEnabler will turn this on, and allow this script to do it's function
+    public bool occupied = false; //the DynamDisabler will turn this of, and allow this script to resume it's function
+    private bool attemptMade;
     
     void Update()
     {
-        if(freeze)
+        if(eligable && !occupied)
         {
-            Freeze();
+            AttemptSpawn();
         }
         else
         {
-            AttemptSpawn();
+            if (attemptMade)
+            {
+                attemptMade = false;
+            }
         }
     }
 
     /// <summary>
-    /// Make a chance based attempt to spawn the prefab. If the chance fails, increase the success rate for next attempt and turn on freeze bool. If the attempt succeeds, reset the success rate and spawn the object
+    /// Make a chance based attempt to spawn the prefab. If the chance fails, increase the success rate for next attempt and turn off eligable bool. If the attempt succeeds, reset the success rate and spawn the object
     /// </summary>
     void AttemptSpawn()
     {
-        int trySpawn = Random.Range(1, (spawnRarity + 1));
-        if(trySpawn <= spawnTarget)
+        if (!attemptMade)
         {
-            spawnTarget = 0;
-            DoSpawn();
+            int trySpawn = Random.Range(1, (spawnRarity + 1));
+            if (trySpawn <= spawnTarget)
+            {
+                spawnTarget = 1;
+                DoSpawn();
+            }
+            else
+            {
+                spawnTarget += 1;
+            }
+            attemptMade = true;
         }
-        else
-        {
-            freeze = true;
-            spawnTarget += 1;
-        }
-    }
-    /// <summary>
-    /// Wait the specified amount of time before turning freeze bool off.
-    /// </summary>
-    void Freeze()
-    {
-        if(spawnDelay <= timePass)
-        {
-            freeze = false;
-            timePass = 0;
-        }
-        else
-        {
-            timePass += Time.deltaTime;
-        }
+        
     }
     /// <summary>
     /// Define the coordinates to the camera's current position and offset to the edge of that camera, then spawn the object
     /// </summary>
     void DoSpawn()
     {
-        Vector3 positionBuffer = new Vector3(this.transform.parent.position.x, this.transform.parent.position.y, 0);
-        Vector3 spawnPosition = positionBuffer;
-        int pickEdge = Random.Range(0, 4);
-        if(pickEdge == 0) //left
-        {
-            spawnPosition += new Vector3(-cameraEdgeX, Random.Range(-cameraEdgeY, cameraEdgeY), 0);
-        }
-        else if(pickEdge == 1) //right
-        {
-            spawnPosition += new Vector3(cameraEdgeX, Random.Range(-cameraEdgeY, cameraEdgeY), 0);
-        }
-        else if(pickEdge == 2) //up
-        {
-            spawnPosition += new Vector3(Random.Range(-cameraEdgeX, cameraEdgeX), -cameraEdgeY, 0);
-        }
-        else if (pickEdge == 3) //down
-        {
-            spawnPosition += new Vector3(Random.Range(-cameraEdgeX, cameraEdgeX), cameraEdgeY, 0);
-        }
-        Instantiate(Object, spawnPosition, Quaternion.identity);
+        Vector3 spawnPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        int pickObject = Random.Range(0, objectList.Length);
+        Instantiate(objectList[pickObject], spawnPosition, Quaternion.identity);
+        occupied = true;
     }
 }
