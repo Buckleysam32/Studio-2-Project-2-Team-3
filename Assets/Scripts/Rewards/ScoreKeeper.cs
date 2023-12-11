de3494176e0ec53eab6fbca118bab625912e97bd
Sam Buckley <1029099@student.sae.edu.au>
2023-12-11 12:06:07 +1000

    Initial character limit

--- Assets/Scripts/Rewards/ScoreKeeper.cs
+++ Assets/Scripts/Rewards/ScoreKeeper.cs
@@ -56,6 +56,7 @@ public class ScoreKeeper : MonoBehaviour
     public void Start()
     {
         // initialize the highscores
+        initialField.characterLimit = 3;
 
         if (PlayerPrefs.HasKey("Highscore0"))
         {
